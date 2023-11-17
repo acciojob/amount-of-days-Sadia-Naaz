110ad8096083b4a536bca789ee2574d40a16cc83
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-17 21:30:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 //your JS code here. If required.
-function  daysOfYear(year) {
-const isLeapYear = ((year%4===0 && year%100!==0)||(year%400===0));
-return isLeapYear?366:365;
-}
-const year = prompt("enter the year");
+function daysOfAYear(year) {
+  // Check if the year is a leap year, considering century years
+  const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 
-	alert(`number of days in year${year} : ${daysOfYear(year)}`);
+  // Return the number of days based on leap year status
+  return isLeapYear ? 366 : 365;
+}
+
+// Take user input for the year
+const userYear = prompt("Enter a year:");
+
+// Convert the user input to a number
+const userYearNumber = parseInt(userYear);
+
+// Check if the input is a valid number
+if (!isNaN(userYearNumber)) {
+  alert(`Number of days in ${userYearNumber}: ${daysOfAYear(userYearNumber)} days`);
+} else {
+ alert("Invalid input. Please enter a valid year.");
+}
