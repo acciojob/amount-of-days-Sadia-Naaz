492714612dee4fe69768f04597399ee6a72e1a37
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-17 20:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function  daysOfYear(year) {
+const isLeapYear = ((year%4===0 && year%100!==0)||(year%400===0));
+return isLeapYear?366:365;
+}
+const year = prompt("enter the year");
+
+	alert(`number of days in year${year} : ${daysOfYear(year)}`);
